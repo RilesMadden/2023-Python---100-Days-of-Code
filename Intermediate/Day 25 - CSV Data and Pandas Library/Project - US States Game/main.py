3c77eebe2803102549dfb19a80419b053b63af49
Riley Madden <rileymadden7@gmail.com>
2023-05-07 16:33:59 -0700

    Changed file names

---
+++ Intermediate/Day 25 - CSV Data and Pandas Library/Project - US States Game/main.py
@@ -0,0 +1,16 @@
+import turtle
+
+screen = turtle.Screen()
+screen.title("U.S. States Game")
+background_image = "Intermediate\Day 25 - CSV Data and Pandas Library\Project - US States Game\states_img.gif"
+screen.addshape(background_image)
+
+turtle.shape(background_image)
+
+
+
+
+
+
+
+screen.exitonclick()
