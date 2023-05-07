e35c5f53728b23eec0aa1769b7ebe2a6af4b990f
Riley Madden <rileymadden7@gmail.com>
2023-05-07 15:10:37 -0700

    Used pandas module to open files

--- Intermediate/Day 25 - CSV Data and Pandas Library/main.py
+++ Intermediate/Day 25 - CSV Data and Pandas Library/main.py
@@ -1,3 +1,17 @@
-with open("Intermediate\Day 25 - CSV Data and Pandas Library\weather_data.csv") as data_file:
-    data = data_file.readlines()
-    print(data)
+# with open("Intermediate\Day 25 - CSV Data and Pandas Library\weather_data.csv") as data_file:
+#     data = data_file.readlines()
+#     print(data)
+
+import csv
+import pandas
+
+# with open("Intermediate\Day 25 - CSV Data and Pandas Library\weather_data.csv") as data_file:
+#     data = csv.reader(data_file)
+#     temperatures = []
+#     next(data_file)
+#     for row in data:
+#         temperatures.append(int(row[1]))
+#     print(temperatures)
+    
+data = pandas.read_csv("Intermediate\Day 25 - CSV Data and Pandas Library\weather_data.csv")
+print(data["temp"])
