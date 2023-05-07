57522645e5f61eedf03c9ce205df99e9860976ff
Riley Madden <rileymadden7@gmail.com>
2023-05-07 15:35:54 -0700

    Added csv data

---
+++ Intermediate/Day 25 - CSV Data and Pandas Library/Squirrel Census Data Analysis/main.py
@@ -0,0 +1,6 @@
+# Create csv file which states fur color and count of squirres
+import pandas
+
+data_csv = pandas.read_csv("Intermediate\Day 25 - CSV Data and Pandas Library\Squirrel Census Data Analysis\2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
+print(data_csv)
+
