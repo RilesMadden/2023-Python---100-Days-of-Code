0da2cc2ec4b623fddc693858789349cefac264ab
Riley Madden <rileymadden7@gmail.com>
2023-05-07 15:33:24 -0700

    Finished lecture and created Squirrel project

--- Intermediate/Day 25 - CSV Data and Pandas Library/main.py
+++ Intermediate/Day 25 - CSV Data and Pandas Library/main.py
@@ -1,17 +1,35 @@
-# with open("Intermediate\Day 25 - CSV Data and Pandas Library\weather_data.csv") as data_file:
-#     data = data_file.readlines()
-#     print(data)
-
-import csv
 import pandas
-
-# with open("Intermediate\Day 25 - CSV Data and Pandas Library\weather_data.csv") as data_file:
-#     data = csv.reader(data_file)
-#     temperatures = []
-#     next(data_file)
-#     for row in data:
-#         temperatures.append(int(row[1]))
-#     print(temperatures)
     
-data = pandas.read_csv("Intermediate\Day 25 - CSV Data and Pandas Library\weather_data.csv")
-print(data["temp"])
+# data = pandas.read_csv("Intermediate\Day 25 - CSV Data and Pandas Library\weather_data.csv")
+
+# # data_dict = data.to_dict()
+# # print(data_dict)
+
+# # temp_list = data["temp"].to_list()
+# # print(temp_list)
+
+# # average_temp = sum(temp_list) / len(temp_list)
+# # print(average_temp)
+
+# # print(data["temp"].max())
+# # print(data.temp)
+
+# #get data in rows
+# print(data[data.day == "Monday"])
+
+# #get data row where temp = max
+# print(data[data.temp == data.temp.max()])
+
+# # Convert Monday's temperature to Fahrenheit
+# monday = data[data.day == "Monday"]
+# print((monday.temp * (9/5) + 32))
+
+#Convert dataframe from scratch
+data_dict = {
+    "students": ["Amy", "James", "Angela"],
+    "scores": [76, 56, 65]
+}
+
+# make csv file
+data = pandas.DataFrame(data_dict)
+data.to_csv("new_data.csv")
