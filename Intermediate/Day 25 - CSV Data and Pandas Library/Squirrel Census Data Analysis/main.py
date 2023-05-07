10857b40b0a5cb49de1e989da8bf7d92b659f0dd
Riley Madden <rileymadden7@gmail.com>
2023-05-07 15:44:25 -0700

    Renamed squirrel data file

--- Intermediate/Day 25 - CSV Data and Pandas Library/Squirrel Census Data Analysis/main.py
+++ Intermediate/Day 25 - CSV Data and Pandas Library/Squirrel Census Data Analysis/main.py
@@ -1,6 +1,9 @@
 # Create csv file which states fur color and count of squirres
 import pandas
+import csv
 
-data_csv = pandas.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
+# data_csv = pandas.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
+# print(data_csv)
+
+data_csv = pandas.read_csv("Intermediate\Day 25 - CSV Data and Pandas Library\Squirrel Census Data Analysis\squirrel_data.csv")
 print(data_csv)
-
--- Intermediate/Day 25 - CSV Data and Pandas Library/main.py
+++ Intermediate/Day 25 - CSV Data and Pandas Library/main.py
@@ -1,35 +1,35 @@
 import pandas
     
-# data = pandas.read_csv("Intermediate\Day 25 - CSV Data and Pandas Library\weather_data.csv")
+data = pandas.read_csv("Intermediate\Day 25 - CSV Data and Pandas Library\weather_data.csv")
+print(data)
+# data_dict = data.to_dict()
+# print(data_dict)
 
-# # data_dict = data.to_dict()
-# # print(data_dict)
+# # # temp_list = data["temp"].to_list()
+# # # print(temp_list)
 
-# # temp_list = data["temp"].to_list()
-# # print(temp_list)
+# # # average_temp = sum(temp_list) / len(temp_list)
+# # # print(average_temp)
 
-# # average_temp = sum(temp_list) / len(temp_list)
-# # print(average_temp)
+# # # print(data["temp"].max())
+# # # print(data.temp)
 
-# # print(data["temp"].max())
-# # print(data.temp)
+# # #get data in rows
+# # print(data[data.day == "Monday"])
 
-# #get data in rows
-# print(data[data.day == "Monday"])
+# # #get data row where temp = max
+# # print(data[data.temp == data.temp.max()])
 
-# #get data row where temp = max
-# print(data[data.temp == data.temp.max()])
+# # # Convert Monday's temperature to Fahrenheit
+# # monday = data[data.day == "Monday"]
+# # print((monday.temp * (9/5) + 32))
 
-# # Convert Monday's temperature to Fahrenheit
-# monday = data[data.day == "Monday"]
-# print((monday.temp * (9/5) + 32))
+# #Convert dataframe from scratch
+# data_dict = {
+#     "students": ["Amy", "James", "Angela"],
+#     "scores": [76, 56, 65]
+# }
 
-#Convert dataframe from scratch
-data_dict = {
-    "students": ["Amy", "James", "Angela"],
-    "scores": [76, 56, 65]
-}
-
-# make csv file
-data = pandas.DataFrame(data_dict)
-data.to_csv("new_data.csv")
+# # make csv file
+# data = pandas.DataFrame(data_dict)
+# data.to_csv("new_data.csv")
